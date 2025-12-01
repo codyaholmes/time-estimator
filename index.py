1f745a4f0b8642ff3f299b015a2198ce4033b73a
cody <codyaholmes@outlook.com>
2025-12-01 15:24:10 -0600

    Update major calculation issues; improve visual elements

--- index.py
+++ index.py
@@ -20,7 +20,7 @@ with form:
     }
     normal_workday = ["M", "T", "W", "Th", "F"]
     week_options = weekday_dict.keys()
-    workdays = st.segmented_control(
+    selected_workdays = st.segmented_control(
         "Which days of the week do you (normally) work?",
         week_options,
         default=normal_workday,
@@ -125,6 +125,7 @@ with form:
             "Weekly Extracurriculars",
             min_value=0.0,
             max_value=40.0,
+            value=15.0,
             step=0.25,
         )
         st.caption(
@@ -133,7 +134,7 @@ with form:
 
     # Check weekly hourly data
     weekly_sleep = sleep * 7
-    weekly_work = len(workdays) * work
+    weekly_work = len(selected_workdays) * work
     weekly_total = weekly_sleep + weekly_work + weekly_extra
     max_weekly = 24 * 7
     free_time = max_weekly - weekly_total
@@ -154,18 +155,23 @@ with form:
         - You sleep about **{weekly_time_text(weekly_sleep)}** a week.
         - You work about **{weekly_time_text(weekly_work)}** a week.
         - You are committed to **{weekly_time_text(weekly_extra)}** of extracurricular activities a week.
-        - You are busy about **{pct_busy_fmttd}** of the week, occupied {weekly_time_text(weekly_total)} of the week.
-        - You are left with **{f":red-background[{weekly_time_text(free_time)}]" if weekly_total > 168 else weekly_time_text(free_time)}** of free time each week.
-    """
+        - You are busy about **{pct_busy_fmttd}** of the week, occupied about {weekly_time_text(weekly_total)} (out of 168)."
+        - You are :green-background[free] **{f":red-background[{weekly_time_text(free_time)}]" if weekly_total > 168 else weekly_time_text(free_time)}** each week.
+    """,
+        unsafe_allow_html=True,
     )
 
+    bad_weekly_data_msg = f"Your weekly inputs are nonsensical. There are only {max_weekly} hours in a week! Readjust."
     if weekly_total > max_weekly:
-        st.error(
-            f"Your weekly inputs are nonsensical. There are only {max_weekly} hours in a week! Readjust."
-        )
+        st.error(bad_weekly_data_msg)
 
 
 with viz:
+    if weekly_total > max_weekly:
+        st.error(
+            "Your weekly inputs are nonsensical. If you can't make sense, why would you expect the visuals too?"
+        )
+
     # PREPARE DATES DATA
     curr_year = datetime.now().year
     year_start = date(curr_year, 1, 1)
@@ -179,32 +185,43 @@ with viz:
 
     # Map work hours to selected days
     def work_hour_map():
-        avg_workday_hours = 0 if len(workdays) == 0 else weekly_work / len(workdays)
-        workdays_long = [weekday_dict.get(day_abbr) for day_abbr in workdays]
+        selected_wd_count = len(selected_workdays)
+        avg_workday_hours = (
+            0 if selected_wd_count == 0 else weekly_work / selected_wd_count
+        )
+        workdays_long = [weekday_dict.get(day_abbr) for day_abbr in selected_workdays]
         return {day: avg_workday_hours for day in workdays_long}
 
     workday_mapping = work_hour_map()
     df["Work"] = df.Weekday.map(workday_mapping).fillna(0)
     df["Extra"] = round(weekly_extra / 7, 2)
-    df["Daily Total"] = df.Sleep + df.Work + df.Extra
-    df["Free Time"] = 24 - df["Daily Total"]
+    df["Occupied"] = df.Sleep + df.Work + df.Extra
+    df["Free"] = 24 - df["Occupied"]
 
     # Calculate yearly sleep totals
     yr_sleep = df.Sleep.sum()
     yr_sleep_avg = 7 * len(df)
     yr_sleep_fmttd = f"~ {yr_sleep:,.0f}"
-    sleep_delta_fmttd = f"{round(yr_sleep - yr_sleep_avg, 1):,.0f}"
+    sleep_delta_fmttd = f"{round(yr_sleep - yr_sleep_avg, 1):,.0f} hrs"
 
     # Calculate yearly work totals
     yr_work_raw = df.Work.sum()
-    yr_offdays = 0 if len(workdays) == 0 else (holidays * work) + (vacation * work)
+    yr_offdays = (
+        0 if len(selected_workdays) == 0 else (holidays * work) + (vacation * work)
+    )
     yr_work = yr_work_raw - yr_offdays
+    workdays_long = [weekday_dict.get(day) for day in selected_workdays]
+    yr_workdays = (
+        0
+        if work == 0 or len(selected_workdays) == 0
+        else len(df.query(f"Weekday in {workdays_long}")) - holidays - vacation
+    )
     normal_workday_long = [weekday_dict.get(day) for day in normal_workday]
     workdays_in_year = len(df.query(f"Weekday in {normal_workday_long}"))
     yr_offdays_avg = (holidays * 8) + (vacation * 8)
     yr_work_avg = (workdays_in_year * 8) - yr_offdays_avg
     yr_work_fmttd = f"~ {yr_work:,.0f}"
-    work_delta_fmttd = f"{round(yr_work - yr_work_avg, 1):,.0f}"
+    work_delta_fmttd = f"{round(yr_work - yr_work_avg, 1):,.0f} hrs"
 
     # Calculate free time
     yr_extra = df.Extra.sum()
@@ -219,7 +236,7 @@ with viz:
     # For the visualization
     pie_df = pd.DataFrame(
         {
-            "Category": ["Free Time", "Sleeping", "Working", "Extracurriculars"],
+            "Category": ["Free", "Sleeping", "Working", "Extra"],
             "Hours": [
                 yr_free_time,
                 yr_sleep,
@@ -232,11 +249,11 @@ with viz:
     # For the dataframe (human friendly)
     pie_df_wide = pd.DataFrame(
         {
-            "Free Time": [yr_free_time],
+            "Free": [yr_free_time],
             "Sleeping": [yr_sleep],
             "Working": [yr_work],
-            "Extracurriculars": [yr_extra],
-            "Max Time in Year": [yr_max_time],
+            "Extra": [yr_extra],
+            "Year Max": [yr_max_time],
         }
     )
 
@@ -251,7 +268,7 @@ with viz:
             textposition="inside",
             textinfo="percent+label",
             textfont_size=14,
-            marker=dict(line=(dict(color="#ffffff", width=1.25))),
+            marker=dict(line=(dict(color="#ffffff", width=1))),
         )
         fig.update_layout(
             showlegend=False, margin=dict(t=margin, l=margin, r=margin, b=margin)
@@ -262,9 +279,12 @@ with viz:
 
     with st.expander("See dates breakdown"):
         st.caption(
-            "Here, you can see how hours are broken down by day across the current year. Although you estimated daily and weekly times on the last screen, there are not 52 weeks perfectly in a year, This table outlines how the hours get distributed across selected working days for this year and then totals them up. So you can be sure that the yearly estimate is fairly close, even if you work M-F and the work week is cut off on Wed. for the current year."
+            "Here, you can see how hours are broken down by day across the current year. Although you estimated daily and weekly times on the last screen, there are not 52 weeks perfectly in a year, This table outlines how the hours get distributed across selected working days for this year and then totals them up. This distribution allows the yearly estimates to be much more precise"
         )
-        st.dataframe(df, hide_index=True, height=300)
+        presentation_df = df.copy()
+        presentation_df["Weekday"] = presentation_df.Weekday.str[:3]
+        presentation_df.rename(columns={"Weekday": "Day"}, inplace=True)
+        st.dataframe(presentation_df, hide_index=True, height=280)
 
     col1, col2 = st.columns(2)
     with col1:
@@ -273,7 +293,7 @@ with viz:
             f"{yr_sleep_fmttd} hrs",
             delta=sleep_delta_fmttd,
             border=True,
-            help="Value below the main metric is the hours of sleep above/below the national yearly average. If metric is green, you sleep X amount more than the U.S. average. If red, then below.",
+            help="Value below the main metric is the hours of sleep above/below the national yearly average. If metric is green, you sleep X amount more than the U.S. average. If red, then below. (Most Americans get about 7 hours a sleep a day.)",
         )
     with col2:
         st.metric(
@@ -287,9 +307,26 @@ with viz:
 
     col1, col2 = st.columns(2)
     with col1:
-        st.metric("Yearly Free Time %", f"{yr_free_time_pct}%", border=True)
+        yr_free_time_fmttd = (
+            f"{yr_free_time_pct:.0f}%"
+            if yr_free_time_pct % 0.25 == 0
+            else f"{yr_free_time_pct:.1f}%"
+        )
+        st.metric("Yearly Free Time %", yr_free_time_fmttd, border=True)
     with col2:
-        st.metric("Working Days in Year", workdays_in_year, border=True)
+        st.metric("Free Days in Year", len(df) - yr_workdays, border=True)
+
+    col1, col2 = st.columns(2)
+    with col1:
+        daily_avg_free = yr_free_time / len(df)
+        daily_avg_free_fmttd = (
+            f"{daily_avg_free:.0f} hrs"
+            if daily_avg_free % 1 == 0
+            else f"{daily_avg_free:.1f} hrs"
+        )
+        st.metric("Avg Daily Free Time", daily_avg_free_fmttd, border=True)
+    with col2:
+        st.metric("Working Days in Year", yr_workdays, border=True)
 
 with info:
     st.markdown(
@@ -298,15 +335,22 @@ with info:
 Cody Holmes
             
 ##### Issues
-Email them to me at codyaholmes@outlook.com. In a future update, I'll put in a short form.
+Email issues to me at codyaholmes@outlook.com. In a future update, I'll put in a form to submit them.
 
 ##### Purpose
-The reason this app came about was because I wanted to get my church brothers and sisters to understand they have much more free time than they think they do. With this free time, they have the ability to read their Bible, memorize verses, or serving others.
+The reason this app came about was because I wanted to get my church peers to understand they have much more free time than they think they do. With this free time, they have the ability to read their Bible, memorize Scripture, or serve others.
                 
-However, the analysis here is general enough that it could apply to anyone, religious and non-religous alike. Most people _convince themselves_ they don't have time to train for that promotion or that they don't have the time to call their loved ones. But you do. And time is our most valuable asset. It's important you understand where it's all going!
+However, the analysis here is general enough that it could apply to anyone, religious and non-religous alike. Most people _convince themselves_ they don't have time to train for that promotion or that they're too busy to call their loved ones. The time is there, I promise. Time is our **most valuable** asset. It's important that you understand where it's all going!
 
-> For everything there is a season, and a time for every matter under heaven. -Ecclesiastes 3:1
+In a future version, I hope to refine the ability to input specific extracurriculars to see how much phones, kids' practices, etc. break out better. _When I have the time_, I'll do so. Terrible pun, I know.
 
-If you like this app and would like to buy me a coffee, don't. Use that money to bless someone else instead. 🙂
+If you like this app and would like to buy me a coffee, don't. Use that money and bless someone else instead. 🙂
+
 """
+    )
+    st.code(
+        '"For everything there is a season, and a time for every matter under heaven."\n- Ecclesiastes 3:1',
+        height="stretch",
+        language="text",
+        wrap_lines=True,
     )
