6db18e2b797f5813086c26c1f5952734a7ab1bc3
cody <codyaholmes@outlook.com>
2025-12-01 14:07:46 -0600

    Push first final draft

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-pg = st.navigation([st.Page("index.py")])
+pg = st.navigation([st.Page("index.py", title="The Free-Time Estimator", icon="🕰️")])
 pg.run()
--- index.py
+++ index.py
@@ -1,17 +1,31 @@
 import streamlit as st
+import pandas as pd
+from datetime import datetime, date
+import plotly.express as px
 
-st.title("Time Estimator")
+st.title("Free-Time Estimator")
 
-tabs = ["Estimator Form", "Free Time Visualization"]
-form, viz = st.tabs(tabs)
+tabs = ["Time Input", "Visualization", "Info"]
+form, viz, info = st.tabs(tabs)
 
 with form:
-    week_options = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+    weekday_dict = {
+        "S": "Sunday",
+        "M": "Monday",
+        "T": "Tuesday",
+        "W": "Wednesday",
+        "Th": "Thursday",
+        "F": "Friday",
+        "Sa": "Saturday",
+    }
+    normal_workday = ["M", "T", "W", "Th", "F"]
+    week_options = weekday_dict.keys()
     workdays = st.segmented_control(
         "Which days of the week do you (normally) work?",
         week_options,
+        default=normal_workday,
         selection_mode="multi",
-        default=["Mon", "Tue", "Wed", "Thu", "Fri"],
+        key="workdays",
     )
 
     sleep_icon = ":material/nightlight:"
@@ -40,7 +54,7 @@ with form:
         )
 
     if sleep <= 0:
-        st.info(f"{sleep_icon} You're not a vampire. Let's be real here, yeah?")
+        st.info(f"{sleep_icon} Even vampires sleep. Let's be real here, yeah?")
     elif sleep <= 2:
         st.error(f"{sleep_icon} Congrats. You're on your way to meet Jesus soon.")
     elif sleep <= 4:
@@ -65,14 +79,6 @@ with form:
 
     col1, col2 = st.columns(2)
     with col1:
-        holidays = st.number_input(
-            "Work holidays in a year",
-            min_value=0,
-            max_value=364,
-            value=10,
-            icon=holidays_icon,
-        )
-    with col2:
         vacation = st.number_input(
             "Vacation days in a year",
             min_value=0,
@@ -80,16 +86,49 @@ with form:
             value=14,
             icon=vacation_icon,
         )
+    with col2:
+        holidays = st.number_input(
+            "Work holidays in a year",
+            min_value=0,
+            max_value=364,
+            value=10,
+            icon=holidays_icon,
+        )
+
+    with st.expander("See a list of US holidays"):
+        st.caption(
+            "Not sure how many work holidays you get in a year? Here's a common list to consider."
+        )
+        st.markdown(
+            """
+            **Major Holidays**
+            - New Year's Day (Jan 1)
+                - New Year's Eve sometimes included
+            - Memorial Day (last Mon in May)
+            - Independence Day (July 4)
+            - Labor Day (first Mon in Sep)
+            - Thanksgiving Day (fourth Thur in Nov)
+                - Some companies inlcude the following Friday
+            - Christmas Day (Dec 25)
+
+            **Less Observed Holidays**
+            - MLK, Jr. Day (thrid Mon in Jan)
+            - Washington's Birthday/President's Day (third Mon in Feb)
+            - Juneteenth (June 19)
+            - Columbus Day (second Mon in Oct)
+            - Veterans Day (Nov 11)
+        """
+        )
 
     with st.container(border=True):
         weekly_extra = st.slider(
-            "Extra weekly hours",
+            "Weekly Extracurriculars",
             min_value=0.0,
             max_value=40.0,
             step=0.25,
         )
         st.caption(
-            "This last input is to capture weekly wild card duties. This could be kids' practices, chores, volunteering, or harrassing your city council. Capture that stuff here. Remember, this is a **weekly** catch-all estimator."
+            "This last input is to capture wild card activities. This could be kids' practices, chores, phone time, exercising, or harrassing your city council. Total it all up (by week) and put it here."
         )
 
     # Check weekly hourly data
@@ -98,7 +137,8 @@ with form:
     weekly_total = weekly_sleep + weekly_work + weekly_extra
     max_weekly = 24 * 7
     free_time = max_weekly - weekly_total
-    pct_busy = weekly_total / max_weekly
+    pct_busy = round(weekly_total / max_weekly * 100, 1)
+    pct_busy_fmttd = f"{pct_busy:.0f}%" if pct_busy % 1 == 0 else f"{pct_busy:.01f}%"
 
     def weekly_time_text(hours):
         hour_part = hours // 1
@@ -114,22 +154,159 @@ with form:
         - You sleep about **{weekly_time_text(weekly_sleep)}** a week.
         - You work about **{weekly_time_text(weekly_work)}** a week.
         - You are committed to **{weekly_time_text(weekly_extra)}** of extracurricular activities a week.
-        - You are busy about **{pct_busy:.1%}** of the week.
-        - You are left with **{weekly_time_text(free_time)}** of free time each week.
+        - You are busy about **{pct_busy_fmttd}** of the week, occupied {weekly_time_text(weekly_total)} of the week.
+        - You are left with **{f":red-background[{weekly_time_text(free_time)}]" if weekly_total > 168 else weekly_time_text(free_time)}** of free time each week.
     """
     )
 
     if weekly_total > max_weekly:
         st.error(
-            f"Your weekly hour inputs are nonsensical. There's only {max_weekly} hours in a week."
+            f"Your weekly inputs are nonsensical. There are only {max_weekly} hours in a week! Readjust."
         )
 
 
-# PREPARE DATES DATA
-
-
-def calculate_weekly_hours(): ...
-
-
 with viz:
-    ...
+    # PREPARE DATES DATA
+    curr_year = datetime.now().year
+    year_start = date(curr_year, 1, 1)
+    year_end = date(curr_year, 12, 31)
+    dates = pd.date_range(year_start, year_end)
+    df = pd.DataFrame(dates, columns=["Dates"])
+    # df["Month"] = df.Dates.dt.strftime("%B")
+    df["Weekday"] = df.Dates.dt.strftime("%A")
+    df["Dates"] = df.Dates.dt.strftime("%m/%d/%Y")
+    df["Sleep"] = sleep
+
+    # Map work hours to selected days
+    def work_hour_map():
+        avg_workday_hours = 0 if len(workdays) == 0 else weekly_work / len(workdays)
+        workdays_long = [weekday_dict.get(day_abbr) for day_abbr in workdays]
+        return {day: avg_workday_hours for day in workdays_long}
+
+    workday_mapping = work_hour_map()
+    df["Work"] = df.Weekday.map(workday_mapping).fillna(0)
+    df["Extra"] = round(weekly_extra / 7, 2)
+    df["Daily Total"] = df.Sleep + df.Work + df.Extra
+    df["Free Time"] = 24 - df["Daily Total"]
+
+    # Calculate yearly sleep totals
+    yr_sleep = df.Sleep.sum()
+    yr_sleep_avg = 7 * len(df)
+    yr_sleep_fmttd = f"~ {yr_sleep:,.0f}"
+    sleep_delta_fmttd = f"{round(yr_sleep - yr_sleep_avg, 1):,.0f}"
+
+    # Calculate yearly work totals
+    yr_work_raw = df.Work.sum()
+    yr_offdays = 0 if len(workdays) == 0 else (holidays * work) + (vacation * work)
+    yr_work = yr_work_raw - yr_offdays
+    normal_workday_long = [weekday_dict.get(day) for day in normal_workday]
+    workdays_in_year = len(df.query(f"Weekday in {normal_workday_long}"))
+    yr_offdays_avg = (holidays * 8) + (vacation * 8)
+    yr_work_avg = (workdays_in_year * 8) - yr_offdays_avg
+    yr_work_fmttd = f"~ {yr_work:,.0f}"
+    work_delta_fmttd = f"{round(yr_work - yr_work_avg, 1):,.0f}"
+
+    # Calculate free time
+    yr_extra = df.Extra.sum()
+    yr_holiday_free_time = 0 if work == 0 else holidays * (24 - work)
+    yr_vacation_free_time = 0 if work == 0 else vacation * (24 - work)
+    yr_max_time = len(df) * 24
+    yr_free_time = yr_max_time - yr_sleep - yr_work - yr_extra
+    yr_free_time_pct = round(yr_free_time / yr_max_time * 100, 1)
+    # st.write(len(df), yr_max_time, yr_free_time)
+
+    # Make new dataframe for pie chart
+    # For the visualization
+    pie_df = pd.DataFrame(
+        {
+            "Category": ["Free Time", "Sleeping", "Working", "Extracurriculars"],
+            "Hours": [
+                yr_free_time,
+                yr_sleep,
+                yr_work,
+                yr_extra,
+            ],
+        }
+    )
+
+    # For the dataframe (human friendly)
+    pie_df_wide = pd.DataFrame(
+        {
+            "Free Time": [yr_free_time],
+            "Sleeping": [yr_sleep],
+            "Working": [yr_work],
+            "Extracurriculars": [yr_extra],
+            "Max Time in Year": [yr_max_time],
+        }
+    )
+
+    with st.container(border=True) as pie_chart:
+        margin = 20
+        st.markdown(
+            "<h6 style='text-align: center'>Yearly Time Breakdown</h6>",
+            unsafe_allow_html=True,
+        )
+        fig = px.pie(pie_df, values="Hours", names="Category")
+        fig.update_traces(
+            textposition="inside",
+            textinfo="percent+label",
+            textfont_size=14,
+            marker=dict(line=(dict(color="#ffffff", width=1.25))),
+        )
+        fig.update_layout(
+            showlegend=False, margin=dict(t=margin, l=margin, r=margin, b=margin)
+        )
+        st.plotly_chart(fig)
+
+        st.dataframe(pie_df_wide, hide_index=True)
+
+    with st.expander("See dates breakdown"):
+        st.caption(
+            "Here, you can see how hours are broken down by day across the current year. Although you estimated daily and weekly times on the last screen, there are not 52 weeks perfectly in a year, This table outlines how the hours get distributed across selected working days for this year and then totals them up. So you can be sure that the yearly estimate is fairly close, even if you work M-F and the work week is cut off on Wed. for the current year."
+        )
+        st.dataframe(df, hide_index=True, height=300)
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.metric(
+            "Yearly Sleep",
+            f"{yr_sleep_fmttd} hrs",
+            delta=sleep_delta_fmttd,
+            border=True,
+            help="Value below the main metric is the hours of sleep above/below the national yearly average. If metric is green, you sleep X amount more than the U.S. average. If red, then below.",
+        )
+    with col2:
+        st.metric(
+            "Yearly Work",
+            f"{yr_work_fmttd} hrs",
+            delta=work_delta_fmttd,
+            delta_color="inverse",
+            border=True,
+            help="Value below the main metric is the hours of work above/below the national yearly average. If metric is red, you work X amount more than the U.S. average. If green, then below.",
+        )
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.metric("Yearly Free Time %", f"{yr_free_time_pct}%", border=True)
+    with col2:
+        st.metric("Working Days in Year", workdays_in_year, border=True)
+
+with info:
+    st.markdown(
+        """
+##### Developer
+Cody Holmes
+            
+##### Issues
+Email them to me at codyaholmes@outlook.com. In a future update, I'll put in a short form.
+
+##### Purpose
+The reason this app came about was because I wanted to get my church brothers and sisters to understand they have much more free time than they think they do. With this free time, they have the ability to read their Bible, memorize verses, or serving others.
+                
+However, the analysis here is general enough that it could apply to anyone, religious and non-religous alike. Most people _convince themselves_ they don't have time to train for that promotion or that they don't have the time to call their loved ones. But you do. And time is our most valuable asset. It's important you understand where it's all going!
+
+> For everything there is a season, and a time for every matter under heaven. -Ecclesiastes 3:1
+
+If you like this app and would like to buy me a coffee, don't. Use that money to bless someone else instead. 🙂
+"""
+    )
